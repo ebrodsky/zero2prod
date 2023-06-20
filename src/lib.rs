fab3c6fc20fe4d5763554a1b25efd9409737f2b3
Eytan Brodsky <eytanbrodsky@gmail.com>
2023-06-20 09:36:15 -0700

    Update -- NOT WORKING

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
+#[cfg(test)]
+mod tests {
+    use crate::routes::health_check;
+
+    #[tokio::test]
+    async fn health_check_works() {
+        let response = health_check().await;
+        assert!(response.status().is_success());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{Connection, PgConnection};
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect Postgres.");
+    let address = format!("localhost:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
